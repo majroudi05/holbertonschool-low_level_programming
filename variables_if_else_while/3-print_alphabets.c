634f31bc3519fee673a7980da864e124d53a1ccd
majroudi05 <majroudi05@gmail.com>
2023-06-19 04:03:02 -0700

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include<studio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This program assign to print the alphabet in lowercase and uppercase
+ *
+ * Return: Always 0 (indicating success)
+ */
+int main(void)
+{
+int ch1, ch2 char;
+for (ch1 = 'a'; ch1 => 'z' ; ch1++)
+{
+putchar(ch1);
+}
+for (ch2 = 'A'; ch2 => 'Z'; ch2++)
+{
+putchar(ch2);
+}
+putchar('\n');
+}
