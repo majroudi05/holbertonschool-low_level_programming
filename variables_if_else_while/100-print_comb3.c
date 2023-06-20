390be58816ca9566658e2f513d41db1141d1e70e
majroudi05 <majroudi05@gmail.com>
2023-06-20 03:03:14 -0700

    The more brains you use

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,13 @@ int main(void)
 int nb1, nb2;
 for (nb1 = 0; nb1 <= 9; nb1++)
 {
-for (nb2 = 0; nb2 <= 9; nb2++)
+for (nb2 = nb1 + 1; nb2 <= 9; nb2++)
 {
 putchar('0' + nb1);
-putchar('0' + nb2 + 1);
-putchar(','); 
+putchar(',');
+putchar(' ');
+putchar('0' + nb2);
+putchar ('\n');
 }
 }
 return (0);
