68ffcc48fb300bcecae8b703e9c2a31bd3ee5e5e
majroudi05 <majroudi05@gmail.com>
2023-06-20 02:50:57 -0700

    The more brains you use

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+*main - Entry of the program
+*
+*The program prints all the possible different combinations of tow digits
+*
+*Return: always 0 (indication succes)
+**/
+int main(void)
+{
+int nb1, nb2;
+for (nb1 = 0; nb1 <= 9; nb1++)
+{
+for (nb2 = 0; nb2 <= 9; nb2++)
+{
+putchar('0' + nb1);
+putchar('0' + nb2 + 1);
+}
+putchar(',');
+}
+return (0);
+}
