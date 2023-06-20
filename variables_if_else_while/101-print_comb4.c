fd0266549cd44009d35610add36f4c1579e2650f
majroudi05 <majroudi05@gmail.com>
2023-06-20 03:37:51 -0700

    Combination for success

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 int nb1, nb2, nb3;
-for (nb1 = 0; nb1 <= 2; nb1++)
+for (nb1 = 0; nb1 <= 9; nb1++)
 {
-for (nb2 = nb1 + 1; nb2 <= 2; nb2++)
+for (nb2 = nb1 + 1; nb2 <= 9; nb2++)
 {
-for (nb3 = nb2 + 1; nb3 <= 2; nb3++)
+for (nb3 = nb2 + 1; nb3 <= 9; nb3++)
 {
 putchar('0' + nb1);
 putchar('0' + nb2);
