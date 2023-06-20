028a247d285aa5be3ac2c006342481976b245c01
majroudi05 <majroudi05@gmail.com>
2023-06-20 03:04:52 -0700

    The more brains you use

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -14,11 +14,10 @@ for (nb1 = 0; nb1 <= 9; nb1++)
 for (nb2 = nb1 + 1; nb2 <= 9; nb2++)
 {
 putchar('0' + nb1);
+putchar('0' + nb2);
+}
 putchar(',');
 putchar(' ');
-putchar('0' + nb2);
-putchar ('\n');
-}
 }
 return (0);
 }
