2bbfc833bdf179184781f41ce6ada58f329193b3
majroudi05 <majroudi05@gmail.com>
2023-06-21 03:51:19 -0700

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+*main - Entry of the program
+*
+*The program prints the alphabet in lowercase
+*
+*Return: always 0(indication of succes)
+**/
+int main(void)
+{
+print_alphabet();
+return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,5 +2,12 @@
 #define MAIN_H
 
 int _putchar(char c);
-
+void print_alphabet(void){
+char ch;
+for(ch = 'a'; ch <= 'z' ; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
 #endif /* MAIN_H */
