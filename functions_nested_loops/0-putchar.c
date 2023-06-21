dcc91977581cf38b705e3e9bfcb9c320e3c404b7
majroudi05 <majroudi05@gmail.com>
2023-06-21 01:42:58 -0700

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+*main - Entry of the prorgram
+*The program print putchar followed by new line
+*Return: always 0 (indication of success)
+**/
+int main(void)
+{
+char ch = ('_putchar');
+putchar(ch);
+putchar('\n');
+return (0);
+}
