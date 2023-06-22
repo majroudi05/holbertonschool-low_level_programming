cd5ae28f021fa50344ee1e04ee31d238871b8f1e
majroudi05 <majroudi05@gmail.com>
2023-06-22 05:09:40 -0700

    the absolut value

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stddef.h>
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
 #endif /* MAIN_H */
