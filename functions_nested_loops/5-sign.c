8a2cec28ae9ade8e01f7090b9e9717656c4fd82f
majroudi05 <majroudi05@gmail.com>
2023-06-22 04:04:59 -0700

    Sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+** print_sign - the program print the sign of a number
+** @n: the character to be checked
+**Return: 1 if @n is grater then zero O if @n is zero and -1 if @n is negative
+**/
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+if else (n == 0)
+{
+return (0);
+}
+else
+{
+return (-1);
+}
+}
