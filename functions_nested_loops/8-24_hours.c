c0af653e783ea03979049b785216ed8f50c850b1
majroudi05 <majroudi05@gmail.com>
2023-06-23 05:58:58 -0700

    Day of my life

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -5,18 +5,26 @@
  **/
 void jack_bauer(void)
 {
-int i;
-int j;
-for (i = 0 ; i < 24 ; i++)
+int i, j, k, l;
+for (i = 0; i <= 2; i++)
 {
-for (j = 0  ; i < 60 ; j++)
+for (j = 0; i <= 9; j++)
 {
-_putchar(('0' + (i / 10)) / 10);
-_putchar(('0' + (i / 10)) % 10);
-_putchar (':');
-_putchar(('0' + (j % 10)) / 10);
-_putchar(('0' + (j % 10)) % 10);
+if ((i <= 1 && j <= 9) || (i <= 2 && i <= 3))
+{
+for (k = 0; k <= 5; k++)
+{
+for (l = 0; l <= 9 ; l++)
+{
+_putchar(i + 'O');
+_putchar(j + '0');
+_putchar(58);
+_putchar(k + '0');
+_putchar(l + '0');
 _putchar('\n');
+}
+}
+}
 }
 }
 }
