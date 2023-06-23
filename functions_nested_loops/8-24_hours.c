0a53a4ebd4cc1495728cd33b8b698a0fd7c8a29d
majroudi05 <majroudi05@gmail.com>
2023-06-23 06:09:05 -0700

    Day of my life

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -16,9 +16,9 @@ for (k = 0; k <= 5; k++)
 {
 for (l = 0; l <= 9 ; l++)
 {
-_putchar(i + 'O');
+_putchar(i + '0');
 _putchar(j + '0');
-_putchar(58);
+_putchar(':');
 _putchar(k + '0');
 _putchar(l + '0');
 _putchar('\n');
