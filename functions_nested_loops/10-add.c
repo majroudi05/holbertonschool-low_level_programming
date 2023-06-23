5792ed1ccb5fc54713e037bfb578795cd78ee4e7
majroudi05 <majroudi05@gmail.com>
2023-06-23 09:47:17 -0700

    add two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include"main.c"
+/**
+*add - a function that adds two integers
+*@x and @y are the two integres of the function
+*Return: the sum of @x and @y
+**/
+int add(int x, int y)
+{
+int z = x + y;
+return (z);
+}
