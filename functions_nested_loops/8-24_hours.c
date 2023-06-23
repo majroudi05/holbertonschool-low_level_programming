4c0a060729aba906d4f44891e3d9162bf25f9530
majroudi05 <majroudi05@gmail.com>
2023-06-23 02:08:16 -0700

    Day of my life

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,10 +1,20 @@
 #include"main.h"
 /**
-*
-**/
+ *jack_bauer - a function that print every minute of the day
+ *Return: nothing
+ **/
 void jack_bauer(void)
 {
-  int i;
-  int j;
-  for (i = 0; i
+int i;
+int j;
+for (i = 0 ; i < 24 ; i++)
+{
+for (j = 0  ; i < 60 ; j++)
+{
+_putchar(i / 10 + '0');
+_putchar (':');
+_putchar(j % 10 + '0');
+_putchar('\n');
+}
+}
 }
