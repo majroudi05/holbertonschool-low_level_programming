7fb4703570608b1a63e265371667ce2452b1e0ed
majroudi05 <majroudi05@gmail.com>
2023-06-27 02:50:16 -0700

    Length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_strlen - function that return the length of a string
+ *@s: the string to be checeked
+ *Return: the length of @s
+ **/
+int _strlen(char *s)
+{
+return (sizeof(*s));
+}
