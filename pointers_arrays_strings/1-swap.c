2ca6b28404fb755d0c12e31801376e0e1b815326
majroudi05 <majroudi05@gmail.com>
2023-06-27 02:40:54 -0700

    dont swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *swap_int - function that swaps the values of tow integers
+ *@a: the first value
+ *@b: the scond value
+ *Return: nothing
+ **/
+void swap_int(int *a, int *b)
+{
+int *s;
+*s = *a;
+*a = *b;
+*b = *s;
+}
