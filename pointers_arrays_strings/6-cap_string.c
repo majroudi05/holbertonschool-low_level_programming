247198ed032f08c15f22fb2235ed0fea2745bb10
majroudi05 <majroudi05@gmail.com>
2023-07-03 02:54:22 -0700

    Expect the best

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include"main.h"
+/**
+ *cap_string - function that capitalizes all words of string
+ *@str: input value
+ *Return: the string after change
+ **/
+char *cap_string(char *str)
+{
+int i = 0;
+while (str[i])
+{
+if (str[i] == ' ' ||
+str[i] == '\t' ||
+str[i] == '\n' ||
+str[i] == ',' ||
+str[i] == ';' ||
+str[i] == '.' ||
+str[i] == '!' ||
+str[i] == '?' ||
+str[i] == '"' ||
+str[i] == '(' ||
+str[i] == ')' ||
+str[i] == '{' ||
+str[i] == '}')
+str[i + 1] = str[i +1] - ('a' - 'A');
+i++;
+}
+return (str);
+}
