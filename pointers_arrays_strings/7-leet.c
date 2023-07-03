5ac99ed5c4bcc78c51d63030c9c70b163e909ed3
majroudi05 <majroudi05@gmail.com>
2023-07-03 03:46:24 -0700

    Mozart

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,29 +8,17 @@ char *leet(char *str)
 {
 int i = 0;
 while (str[i])
+{
 if (str[i] == 'a' || str[i] == 'A')
-{
-str[i] = 4;
-i++;
-}
+str[i] = '4';
 else if (str[i] == 'e' || str[i] == 'E')
-{
-str[i] = 3;
-i++;
-}
+str[i] = '3';
 else if (str[i] == 'o' || str[i] == 'O')
-{
-str[i] = 0;
-i++;
-}
+str[i] = '0';
 else if (str[i] == 't' || str[i] == 'T')
-{
-str[i] = 7;
-i++;
-}
+str[i] = '7';
 else if (str[i] == 'l' || str[i] == 'L')
-{
-str[i] = 1;
+str[i] = '1';
 i++;
 }
 return (str);
