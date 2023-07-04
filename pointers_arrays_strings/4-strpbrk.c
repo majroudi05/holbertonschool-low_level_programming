82925195c97548cc6eddd27e30d1b725cd06ea55
majroudi05 <majroudi05@gmail.com>
2023-07-04 05:41:40 -0700

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ *_strpbrk - function that searches a string of a set of bytes
+ *@s: input value
+ *@accept: input value
+ *Return: a pointer to the byte in @s
+ **/
+char *_strpbrk(char *s, char *accept)
+{
+int i, n;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (n = 0; accept[n] != '\0'; n++)
+{
+if (s[i] == accept[n])
+return (s + i);
+}
+}
+return (NULL);
+}
