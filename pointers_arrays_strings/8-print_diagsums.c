850e66bcaebdb12bcef242e9c2ddc83734c2b465
majroudi05 <majroudi05@gmail.com>
2023-07-05 03:24:27 -0700

    diagonal

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,7 @@ int sum1 = 0;
 int sum2 = 0;
 for (i = 0; i <= (size * size); i = i + size + 1)
 sum1 = sum1 + a[i];
-for (j = size; j <= (size * size) - size; j = j + size - 1)
+for (j = size - 1; j <= (size * size) - size; j = j + size - 1)
 sum2 = sum2 + a[j];
 printf("%d, %d\n", sum1, sum2);
 }
