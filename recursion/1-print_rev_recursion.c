d64876f7ea1a8b84de45381906e145dc0208940c
majroudi05 <majroudi05@gmail.com>
2023-07-05 15:09:19 -0700

    string in reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#includ"main.h"
+/**
+ *_print_rev_recursion - Function that print string in reverse
+ *@s: input value
+ *Return: nothing
+ **/
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+s++;
+_print_rev_recursion(char *s);
+s--;
+putchar(*s)
+}
