ab37d42a06b04412ecd5369d59c07198ed4b1621
majroudi05 <majroudi05@gmail.com>
2023-07-06 03:34:36 -0700

    Raised

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ *_pow_recursion - function that return the @x raised to power of @y
+ *@x: input value
+ *@y: input value
+ *Return: the raised of @x by @y
+ **/
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+{
+return (-1);
+}
+return (x * _pow_recursion(x, y - 1));
+}
