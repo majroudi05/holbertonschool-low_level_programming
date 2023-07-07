f8b692a4f2e59e5868f4e1271f0df5c1a0eb9cf6
majroudi05 <majroudi05@gmail.com>
2023-07-07 14:25:53 -0700

    arg

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - the entry of the program
+*@argc: input value
+*@argv: input value
+*Return: always 0
+**/
+int main(int argc, char *argv[])
+{
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2]);
+int result = num1 * num2;
+printf("%d\n", result);
+return (0);
+}
