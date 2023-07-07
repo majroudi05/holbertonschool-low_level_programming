25a06b6aa200705cdba0beaf5eca8037c0fc55ba
majroudi05 <majroudi05@gmail.com>
2023-07-07 14:11:00 -0700

    arg

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Entry of the program
+ *@argc: input value
+ *@argv: input value
+ *Return: always 0
+ **/
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
