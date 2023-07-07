c2e985071f98966ae047b33b477b1faf18ce7011
majroudi05 <majroudi05@gmail.com>
2023-07-07 14:06:57 -0700

    arg

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,10 +3,11 @@
  * main - the Entry
  *@argv: input value
  *@argc: input value
- *Return: nothing
+ *Return: Always 0
  **/
 int main(int argc, char *argv[])
 {
+(void) argc;
 printf("%s\n", argv[0]);
 return (0);
 }
