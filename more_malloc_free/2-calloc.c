ba6fedacc3eee82369059ef906760acb7c3aa626
majroudi05 <majroudi05@gmail.com>
2023-07-20 03:09:09 -0700

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include<stdlib.h>
+/**
+ *_calloc - function that allocates memory
+ *@nmemb: input value
+ *@size: input value
+ *Return: pointer
+ **/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+if (nmemb == 0 || size == 0)
+return (NULL);
+void *p = malloc(nmemb * size);
+if (p == NULL)
+{
+return (NULL);
+}
+memset(p, 0, nmemb * size);
+return (p);
+}
