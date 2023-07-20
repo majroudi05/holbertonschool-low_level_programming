1cb8d41d86aee12767e8eb5db8a7d36fb7347158
majroudi05 <majroudi05@gmail.com>
2023-07-20 03:14:10 -0700

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 if (nmemb == 0 || size == 0)
 return (NULL);
-void *p = malloc(nmemb * size);
+void* p = malloc(nmemb * size);
 if (p == NULL)
 {
 return (NULL);
