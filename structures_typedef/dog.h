c9cfb58bc7db689d19e674dacce45314f2bb8053
majroudi05 <majroudi05@gmail.com>
2023-07-21 01:39:39 -0700

    poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifdef MY_MACRO_H
+#defiine MY_MACRO_H
+/**
+ *struct dog - dog attributes
+ *@name: name of the dog
+ *@age: the age of the dog
+ *@owner: the owner of the dog
+ **/
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
