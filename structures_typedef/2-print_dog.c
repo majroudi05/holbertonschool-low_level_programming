e5860736253c0b0fd974593d68f342c4fdee55da
majroudi05 <majroudi05@gmail.com>
2023-07-21 03:01:06 -0700

    init dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include"dog.h"
+#include<stdlib>
+/**
+ *print_dog - print all the dog attributes
+ *@d: the dog struct
+ *Return: nothing
+ *
+ **/
+void print_dog(struct dog *d)
+{
+if (d != NULL)
+{
+printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+printf("Age: %s\n", (d->age) ? d->age : 0);
+printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+}
+}
