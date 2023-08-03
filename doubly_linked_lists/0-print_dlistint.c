d07c7bb6d647e298d19652af4605638987c788da
majroudi05 <majroudi05@gmail.com>
2023-08-03 13:02:23 +0100

    new function that print all elements of a double linked list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,10 +8,11 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 size_t size = 0;
-if ( h != NULL)
+while (h != NULL)
 {
-printf("%d\n", h->n);
 size++;
+printf("%d\n", h->n);
+h = h->next;
 }
 return (size);
 }
