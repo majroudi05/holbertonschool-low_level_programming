21621aab5b6e42f46728429c5cc770158664802d
majroudi05 <majroudi05@gmail.com>
2023-08-03 09:47:49 +0100

    add new function that add new node to the linked list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include"lists.h"
+/**
+*add_node - function that  add a new node
+*@head: the head node
+*@str: the new str that we should add in the head
+*Return: the adresse on the new element
+**/
+list_t *add_node(list_t **head, const char *str)
+{
+char *duplicate = strdup(str);
+if (str == NULL)
+return (NULL);
+list_t *new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+if (duplicate == NULL)
+{
+free(new_node);
+return (NULL);
+}
+else
+{
+for (i = 0; duplicate[i]; i++)
+{
+new_node->str = duplicate;
+new_node->len = i + 1;
+new_node->next = *head;
+}
+}
+*head = new_node;
+return (new_node);
+}
