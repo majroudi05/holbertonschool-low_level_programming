62d0f29cbae2799cb809a024e245a3f6d7bc08fa
majroudi05 <majroudi05@gmail.com>
2023-08-03 16:25:54 +0100

    create function that free all the node of linked list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include"lists.h"
+/**
+*free_dlistint - Function that free the linked list
+*@head: pointer to the head of the list
+*Return: nothing
+*
+**/
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *current;
+while (head != NULL)
+{
+current = head->next;
+free(head->n);
+free(head->prev);
+free(head);
+head = current;
+}
+}
