387703f3a48ece5230f2fa8e5a00a18caba6d538
majroudi05 <majroudi05@gmail.com>
2023-08-03 08:56:18 +0100

    add new file list_len which return node number of linked list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include"lists.h"
+/**
+*list_len - function that returns number of elements in the linked list
+*@h: the linked list
+*Return: number of element
+*
+**/
+size_t list_len(const list_t *h)
+{
+    size_t length = 0;
+    while ( h != NULL)
+    {
+        size++;
+        h = h->next;
+    }
+    return (length);
+}
