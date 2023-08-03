ec6721a070e59cc70c37a47884e9a3fd6256d954
majroudi05 <majroudi05@gmail.com>
2023-08-03 08:39:33 +0100

    betty change

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,26 +1,26 @@
 #include"lists.h"
 #include<stddef.h>
 /**
-*prin_list - Function that print all the elements in linked list
+*print_list - Function that print all the elements in linked list
 *@h: liked list
 *Return: the number of node of the linked list
 *
 **/
 size_t print_list(const list_t *h)
 {
-   size_t size = 0;
-   while (h != NULL)
-   {
-    if (h->str == NULL)
-    {
-        printf("[0] (nil)\n");
-    }
-    else
-    {
-    printf("[%u] %s\n", h->len, h->str);
-    }
-    size ++;
-    h = h->next;
-   }
-   return (size);
+size_t size = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+}
+size++;
+h = h->next;
+}
+return (size);
 }
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,8 +1,8 @@
-#ifdef MY_LINKED_LIST
+#ifndef MY_LINKED_LIST
 #define MY_LINKED_LIST
 #include<stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -12,9 +12,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 #endif
