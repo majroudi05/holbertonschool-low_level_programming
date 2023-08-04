1fe95a49062365923efce822119292ba3166db28
majroudi05 <majroudi05@gmail.com>
2023-08-04 11:48:38 +0100

    add new fucntion that add node in linked list

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,30 @@
+#include"lists.h"
+/**
+*insert_dnodeint_at_index - Function that add a node in idx
+*@h: the headd pointer of linked list
+*@idx: the position of the add node
+*@n: the value of the new node
+*Return: the linked list
+**/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+dlistint_t *new = malloc(sizeof(dlistint_t));
+dlistint_t *temp = *h;
+unsigned int i;
+new->n = n;
+new->prev = NULL;
+new->next = NULL;
+while (temp != NULL)
+if (i == idx - 1)
+{
+new->next = temp->next;
+temp->next = new;
+break;
+}
+else
+{
+i++;
+temp = temp->next;
+}
+return (h);
+}
