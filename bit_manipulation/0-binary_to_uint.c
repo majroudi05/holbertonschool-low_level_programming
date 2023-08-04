27bd4020ea2e4036dd8cf31c1a77c1906e3bd096
majroudi05 <majroudi05@gmail.com>
2023-08-04 16:05:30 +0100

    function that converts a binary number to an unsigned int

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * binary_to_uint - Function that converts a binary number
+ * @b: pointer to string
+ * Return: the convert number
+**/
+unsigned int binary_to_uint(const char *b)
+{
+int i;
+unsigned int dec = 0;
+if (!b)
+return (0);
+for (i = 0; b[i]; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+return (0);
+dec = 2 * dec + (b[i] - '0');
+}
+return (dec);
+}
---
+++ bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef MY_BIT
+#define MY_BIT
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+unsigned int binary_to_uint(const char *b);
+#endif
