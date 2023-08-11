1e8fc193417b8ef83dc6974c4e611a874168de4f
majroudi05 <majroudi05@gmail.com>
2023-08-11 10:40:49 +0100

    how to read and print a text

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -7,19 +7,27 @@
 **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-size_t f;
-size_t w;
-size_t r;
+FILE *f = fopen(filename, "r");
 char *buf;
-f = open(filename, O_RDONLY);
-if (f == -1)
+size_t r;
+size_t w;
+if (filename == NULL)
 {
 return (0);
 }
-buf = malloc(sizeof(char) * letters);
-r = read(f, buf, letters);
-w = write(STDOUT_FILENO, buf, r);
+if (f == NULL)
+{
+return (0);
+}
+buf = (char *)malloc(letters + 1);
+if (buf == NULL)
+{
+fclose(f);
+return (0);
+}
+r = fread(buf, sizeof(char), letters, f);
+w = fwrite(buf, sizeof(char), r, stdout);
 free(buf);
-close(f);
+fclose(f);
 return (w);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,7 @@
 #ifndef MY_FILE
 #define MY_FILE
+#include<stdio.h>
+#include<stdlib.h>
 #include<unistd.h>
 #include<fcntl.h>
 ssize_t read_textfile(const char *filename, size_t letters);
